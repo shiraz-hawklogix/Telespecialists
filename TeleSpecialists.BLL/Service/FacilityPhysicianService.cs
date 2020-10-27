7d9d3b4998e4177690f709a2a4563c34135276b0
afaf-hawklogix <71007243+afaf-hawklogix@users.noreply.github.com>
2020-10-27 13:14:56 +0500

    Revert "Revert "Revert "Revert "remove token on signout and send only request to logged in user""""
    This reverts commit 9c52ace746705a4d2000f283de92adb9f02f1a2e.

--- TeleSpecialists.BLL/Service/FacilityPhysicianService.cs
+++ TeleSpecialists.BLL/Service/FacilityPhysicianService.cs
@@ -604,6 +604,7 @@ namespace TeleSpecialists.BLL.Service
                     result = result.Where(m => Physicians.Contains(m.fap_user_key));
             }
             #endregion
+
             var finalresult = result.Select(x => new
             {
                 id = x.fap_key,
