f864a0510806d4f5a1a979feca43cac9ade733b7
afaf-hawklogix <71007243+afaf-hawklogix@users.noreply.github.com>
2020-11-11 17:05:16 +0500

    lookup controller changes

--- TeleSpecialists.BLL/Service/LookupService.cs
+++ TeleSpecialists.BLL/Service/LookupService.cs
@@ -50,6 +50,7 @@ namespace TeleSpecialists.BLL.Service
         public IQueryable<facility> GetStrokeFacilitiesForOthercasetypes(string phoneNumber)
         {
             return GetFacilities(phoneNumber).Where(f => f.fac_go_live);
+        }
         public IQueryable<facility> GetFacilityAll(string phoneNumber)
         {
             return GetFacilities(phoneNumber);
