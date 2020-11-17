544dbe815764032d5cba5272341ec5a68f5aca89
Ahmad Junaid <71013244+junaid-hawklogix@users.noreply.github.com>
2020-11-17 18:26:46 +0500

    Showblast in dashboard
    Showblast in dashboard

---
+++ TeleSpecialists.BLL/ViewModels/StrokeStatChartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeleSpecialists.BLL.ViewModels
+{
+    public class StrokeStatChartViewModel
+    {
+        public int Hour { get; set; }
+        public int Count { get; set; }
+        public int PhysicianBlastCount { get; set; }
+        public int NavigatorBlastCount { get; set; }
+        public int STATCount { get; set; }
+    }
+}
